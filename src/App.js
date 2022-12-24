47f42b941895a8bc6b08f250665dc2aaa2057da8
Nanda Prabhu B <75189847+Nanda-prabhu@users.noreply.github.com>
2022-12-24 16:47:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Cards from "./components/Cards";
+import Search from "./components/Search";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
+  //now whenever term changes this fetch request will run again
+  // empty array at the end for dependencies 
+
+  return(
+    <div className="container mx-auto lg:ml-12">
+
+      <Search searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl mx-auto text-center mt-32 text-gray-600">No Images Found </h1> }
+
+      {isLoading? <h1 className="text-6xl mx-auto text-center mt-32 text-gray-600">Loading... Hang on a sec.</h1> : <div className="grid grid-cols-3 gap-4 lg:gap-2 lg:px-12">
+        {images.map(image => (
+          <Cards key={image.id} image={image}/> 
+        ))}
+      </div>}
+
+  </div>
+  )
+
+} 
 
 export default App;
